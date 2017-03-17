87feb12b6fdf8a334b27f5f38cf03632d3114be2
me <myuweb@gmail.com>
2017-03-17 15:10:46 +0800

    jquery sidebar plugin
    left or right animate slide menu;
    need jquery&bootstrip

---
+++ aside.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+  $('.show_patient_detial').aside({
+    target:'.patient_detail',
+      closebutton:'.closeaside',
+      callback:function(){
+        console.log($(this).data('id'));
+      }
+    });
+
+
+});
+
+(function( $ ) {
+    $.fn.aside = function(options){
+        var defaults = {
+            target: '', //.aside_name
+            toggle:true,
+            closebutton:'', //.closebutton_name
+            callback: function(){}
+
+        };
+
+        var options  = $.extend( {}, defaults, options );
+
+
+        return this.each(function() {
+
+            var me = $(this);
+
+            //me.on('click', toggleSide);
+
+            $(document).on('click',hideSide);
+
+            //closetarget(me);
+
+            me.on('click',toggleSide)
+
+        });
+
+        function hideSide(e){
+            var that = $(e.target);
+            //console.log(that.is(options.target));
+            if(that.is('[data-toggle="aside"]')){
+                return;
+            }
+            if(that.is(options.target)){
+                return;
+            }
+            if(that.is(options.closebutton)){
+                $(options.target).removeClass('sidebar-open');
+            }
+            else{
+                $(options.target).removeClass('sidebar-open');
+            }
+        }
+
+
+        function toggleSide(e){
+            $.isFunction(options.callback) && options.callback.call(this);
+            var that = $(e.target);
+
+            if(that.is('[data-toggle="aside"]') || that.is(options.target)){
+                $(options.target).hasClass('sidebar-open') ? '' : $(options.target).addClass('sidebar-open');
+                return;
+            }
+
+
+        }
+
+    }
+})( jQuery );
